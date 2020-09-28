7f6bb2956bba9359468267126e5782ee77bd1b78
Adrián <avazmir1337@gmial.com>
2020-09-28 12:51:24 +0200

    Cambios en el componente Home para cargar los últimos artículos.

--- AprendiendoAngular/src/app/components/home/home.component.ts
+++ AprendiendoAngular/src/app/components/home/home.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from '../../services/article.service';
+import { Article } from '../../models/article';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ArticleService]
 })
 export class HomeComponent implements OnInit {
 
   public titulo: string;
+  public articles: Array<Article>;
 
-  constructor() { 
+  constructor( private _articleService: ArticleService ) { 
     this.titulo = 'Últimos artículos';
   }
 
   ngOnInit(): void {
+
+      //Lamada AJAX al Backend
+      //pasando a getArticles true, o cualquiero otra cosa, decimos que nos saque los ultimos articulos definidos en nuestro servicio
+      this._articleService.getArticles(true).subscribe(
+        response => {
+          if(response.articles){
+            this.articles = response.articles;
+          }
+        },
+        error => {
+          console.log(error);
+        }
+    );
   }
+  
 
 }
