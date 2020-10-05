c24e93dc93a17923122b4c6de28ebfb7355931dc
Adrián <avazmir1337@gmial.com>
2020-10-05 14:01:22 +0200

    Creación de componente para error 404

---
+++ AprendiendoReact/src/components/Error.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h2 className="sub-header">Página no encontrada</h2>
+            <p>
+            Vaya, parece que la página a la que intentas acceder no existe o no está disponible en este momento, 
+            por favor, intentelo más tarde y disculpe las molestias.
+            </p>
+            <b>:)</b>
+        </div>
+    );
+};
+
+export default Error;
