c62f439daf15ea985e1c6b39062dccc98e5f5573
Adrián <avazmir1337@gmial.com>
2020-10-08 14:35:07 +0200

    Añadida funcionalidad al componente Search

--- AprendiendoReact/src/components/Sidebar.js
+++ AprendiendoReact/src/components/Sidebar.js
@@ -1,8 +1,36 @@
 import React from 'react';
+//Importamos Redirect
+import { Redirect } from 'react-router-dom';
 
 class Sidebar extends React.Component {
 
+    searchRef = React.createRef();
+
+    state = {
+        search: '',
+        redirect: false
+    }
+
+
+    redirectToSearch = (e) =>{
+        //bloquemos el evento para que no recargue la página
+        e.preventDefault();
+        
+        //Damos valos al state
+        //IMPORTANTE this.searchRef.CURRENT.VALUE
+        this.setState({
+            search: this.searchRef.current.value,
+            redirect: true
+        });
+    }
+
     render() {
+        //La redirección debemos hacerla dentro del Render
+        if(this.state.redirect){
+            return (
+                <Redirect to={ '/redirect/' + this.state.search } />
+            );
+        }
 
         return (
             <aside id="sidebar">
@@ -10,15 +38,15 @@ class Sidebar extends React.Component {
                     this.props.blog === "true" &&
                     <div id="nav-blog" className="sidebar-item">
                         <h3>Puedes hacer esto</h3>
-                        <a href="#" className="btn btn-success">Crear Artículo</a>
+                        <button  className="btn btn-success">Crear Artículo</button>
                     </div>
                 }
                 
                 <div id="search" className="sidebar-item">
                     <h3>Buscador</h3>
                     <p>Encuentra el artículo que buscas.</p>
-                    <form>
-                        <input type="text" name="search"/>
+                    <form onSubmit={ this.redirectToSearch }>
+                        <input type="text" name="search" ref={ this.searchRef }/>
                         <input type="submit" className="btn" name="submit" value="Buscar"/>
                     </form>
                 </div>
