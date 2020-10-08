8f42b198ef145066a2513ed8cad45fec86b072ee
Adrián <avazmir1337@gmial.com>
2020-10-08 14:36:46 +0200

    Añadida funcionalidad para el componente Search

---
+++ AprendiendoReact/src/components/Search.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import Slider from './Slider';
+import Sidebar from './Sidebar';
+
+import Articles from './Articles';
+
+class Search extends Component{
+    render(){
+
+        //Recogemos el string a buscar, (IMPORTANTE: this.props.match.params para acceder al pará,etro por URL);
+        var searched = this.props.match.params.search;
+        
+        return (
+            <>
+            <Slider
+                title={ 'Busqueda: ' + searched }
+                size="slider-small"
+            />
+            <div className="center">
+                <section id="content">
+                    {/* Listado de artículos que vendrán del API-REST */}
+                    <Articles search={ searched }/>
+                </section>
+                <Sidebar
+                    blog="true"
+                />
+            </div>
+        </>
+        );
+    }
+}
+export default Search;
