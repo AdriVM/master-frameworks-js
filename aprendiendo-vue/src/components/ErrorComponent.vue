c23774240f011e66655ca3fc5b65d1f1d40c931f
Adrián <avazmir1337@gmial.com>
2020-10-20 11:56:22 +0200

    Creación de componente de Error

---
+++ aprendiendo-vue/src/components/ErrorComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <section id="content">
+    <h1 class="sub-header">Página no encontrada</h1>
+    <p>
+      Vaya, parece que algo ha ido mal en tu búsqueda, intenteló más tarde y
+      disculpe las molestias.
+    </p>
+    <p>
+      <b>:)</b>
+    </p>
+  </section>
+</template>
+<script>
+export default {
+  name: "ErrorComponent",
+};
+</script>
