661d272339211e7bb65d4af479da4802dd74e9a3
Adrián <avazmir1337@gmial.com>
2020-10-20 11:55:59 +0200

    Creación de componente de Blog

---
+++ aprendiendo-vue/src/components/BlogComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <section id="content">
+      <h2 class="sub-header">Blog</h2>
+    </section>
+</template>
+<script>
+
+export default {
+  name: "BlogComponent",
+  components: {
+
+  },
+};
+</script>
