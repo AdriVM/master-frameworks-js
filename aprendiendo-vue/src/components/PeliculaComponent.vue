98efda6c1d54ad4e5db9d4125f080e7dff315ad7
Adrián <avazmir1337@gmial.com>
2020-10-27 13:56:14 +0100

    Creación del componente Pelicula, comunicación entre componente padre e hijo, emisión del evento favorita al componente padre

---
+++ aprendiendo-vue/src/components/PeliculaComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <article class="article-item" id="article-template">
+        <div class="image-wrap">
+            <!-- v-bind: o : aseacas, es lo mismo -->
+          <img
+            v-bind:src="pelicula.image"
+            v-bind:alt="pelicula.title"
+            :title="pelicula.title"
+            />
+        </div>
+
+        <h2>{{ pelicula.title }}</h2>
+        <span :class="{
+            dateyellow: pelicula.year < 2015,
+            dategreen: pelicula.year >= 2015,
+            datered: pelicula.year == 2017
+        }"> {{ pelicula.year }} </span>
+        <a href="article.html">Leer más</a>
+        <!-- @click="" o v-on:click=""-->
+        <input type="button" value="Marcar como favorita"
+            @click="pasarFavoritaAlPadre(pelicula)"
+        />
+        <div class="clearfix"></div>
+      </article>
+</template>
+<script>
+export default {
+    name: 'Pelicula',
+    props: ['pelicula'],
+    methods: {
+        pasarFavoritaAlPadre(pelicula){
+            //Emitimos la pelicula al elemento padre
+            this.$emit('favorita', pelicula)
+        }
+    }
+    
+}
+</script>
