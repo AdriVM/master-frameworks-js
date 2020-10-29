16291ec32bce0b81443aaf0a0887d2f204398da7
Adrián <avazmir1337@gmial.com>
2020-10-29 14:13:45 +0100

    Creación del componente Articles que muestra un listado de los artículos recibidos en las props

---
+++ aprendiendo-vue/src/components/ArticlesComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="article-list">
+    <article
+      class="article-item"
+      v-for="article in articles"
+      :key="article._id"
+    >
+      <div class="image-wrap">
+        <img
+          :src="url + 'get-image/' + article.image"
+          :alt="article.title"
+          v-if="article.image"
+        />
+        <img
+          src="../assets/images/default.png"
+          :alt="article.title"
+          v-if="!article.image"
+        />
+      </div>
+
+      <h2>{{ article.title }}</h2>
+      <span class="date"> {{ article.date }} </span>
+      <a href="article.html">Leer más</a>
+      <div class="clearfix"></div>
+    </article>
+  </div>
+</template>
+<script>
+import Global from '../Global';
+
+export default {
+  name: "ArticlesComponent",
+  props: ["articles"],
+  data(){
+      return{
+          url: Global.url
+      }
+  }
+};
+</script>
