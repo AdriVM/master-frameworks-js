f42b22c337e72746bf08ccd64c4784526b8c0558
Adrián <avazmir1337@gmial.com>
2020-10-29 14:17:44 +0100

    Creación de archivo Global que guarda la url del backend y cambios provocados por la instalación de axios

---
+++ aprendiendo-vue/src/Global.js
@@ -0,0 +1,11 @@
+/*
+//Exportación normal, requiere ser importado con {}
+export var Global = {
+    url: 'http://localhost:3900/api/'
+}*/
+
+//Exportacion por default, se importa como un componente
+var Global = {
+    url: 'http://localhost:3900/api/'
+}
+export default Global;
