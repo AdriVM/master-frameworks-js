6bb38396aab81e4a835c76230469fea7621e687a
Adrián <avazmir1337@gmial.com>
2020-11-10 12:02:38 +0100

    Añadidos todos los componentes nuevos, las url a dichos componentes así como la libreria de moment para el formateo de fechas

--- aprendiendo-vue/src/main.js
+++ aprendiendo-vue/src/main.js
@@ -11,6 +11,12 @@ import BlogComponent from './components/BlogComponent.vue';
 import FormularioComponent from './components/FormularioComponent.vue';
 import PaginaComponent from './components/PaginaComponent.vue';
 import ErrorComponent from './components/ErrorComponent.vue';
+import BuscadorComponent from './components/BuscadorComponent.vue';
+//Ruta para la redireccion del buscador
+import Redirect from './components/Redirect.vue';
+import ArticleComponent from './components/ArticleComponent.vue'
+import CrearArticuloComponent from './components/CrearArticuloComponent.vue';
+import EditarArticuloComponent from './components/EditarArticuloComponent.vue';
 
 
 import MiComponente from './components/MiComponente.vue';
@@ -23,15 +29,32 @@ Vue.use(VueRouter);
 //Hacemos que vue haga uso del Vuelidate
 Vue.use(Vuelidate);
 
+//Ponemos moment en castellano, estos require 
+//pueden ser imports para que quede solo con el use, 
+//incluso el require de vue-moment, puede ser importado
+// y pasado al use tal como hicimos con el vuelidate
+const moment = require('moment');
+require('moment/locale/es');
+//Ahora se lo pasamos al vue.use
+Vue.use(require('vue-moment'), {
+  moment
+})
+
 //Definimos las rutas
 const routes = [
   { path: '/home', component: UltimosArticulos },
   { path: '/blog', component: BlogComponent },
+  { path: '/articulo/:id', name: 'article', component: ArticleComponent },
   { path: '/formulario', component: FormularioComponent },
   { path: '/pruebas/:id?', name: 'pruebas' ,component: PaginaComponent },//El name se utiliza para hacer enlaces y otras cosas por el estilo
   //la ? es para hacer opcional el parámetro
 
   { path: '/ultimos-articulos', component: UltimosArticulos },
+  { path: '/busqueda/:searchString', component: BuscadorComponent },
+  { path: '/crear-articulo', name: "create", component: CrearArticuloComponent },
+  { path: '/editar-articulo/:id', component: EditarArticuloComponent },
+
+  { path: '/redirect/:searchString', component: Redirect },
   { path: '/mi-componente', component: MiComponente },
   { path: '/hola-mundo', component: HolaMundo },
   { path: '/peliculas', component: Peliculas },
